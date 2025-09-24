2cd7a10e61c8ffc92858a95f2dd4f092baff2953
b <basarkis@icloud.com>
2025-09-24 13:27:49 -0400

    Add playback functionality

--- main.py
+++ main.py
@@ -26,13 +26,28 @@ def search_and_play_song(ytmusic):
     url = f"https://music.youtube.com/watch?v={selected_song['videoId']}"
     print(f"Playing: {selected_song['title']} by {selected_song['artists'][0]['name']}")
 
-    subprocess.run(
+    # Open yt-dlp process to fetch audio
+    ytdlp = subprocess.Popen(
         ["yt-dlp", "-q", "-o", "-", "-f", "bestaudio", url],
         stdout=subprocess.PIPE
     )
 
+    # Open ffplay process to play audio
+    ffplay = subprocess.Popen(
+        ["ffplay", "-nodisp", "-autoexit", "-loglevel", "quiet", "-"],
+        stdin=ytdlp.stdout
+    )
+
+    # Make sure yt-dlp's stdout is connected to ffplay
+    ytdlp.stdout.close()
+
+    # Wait until ffplay finishes playing
+    ffplay.wait()
+
+
+
 def main():
-    ytmusic = YTMusic(headers_auth.json)
+    ytmusic = YTMusic("headers_auth.json")
 
     while True:
         choice = inquirer.select(
